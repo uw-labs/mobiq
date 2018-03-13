3cf4a94eaea45238debbe07735a81153c4e35d12
Michael Donat <michael.donat@me.com>
2018-03-13 16:03:04 +0000

    Do not rewrite array keys

--- transform/sanitiseNames.js
+++ transform/sanitiseNames.js
@@ -3,7 +3,11 @@ const {Transform} = require('stream');
 const sanitiseNames = (obj) => {
     Object.keys(obj).forEach(key => {
 
-        const newKey = key.replace(/[^a-zA-Z0-9]/g, '_').replace(/^([^a-zA-Z])(.+)/, '_$1$2')
+        let newKey = key
+
+        if (!Array.isArray(obj)) {
+            newKey = key.replace(/[^a-zA-Z0-9]/g, '_').replace(/^([^a-zA-Z])(.+)/, '_$1$2')
+        }
 
         if (newKey !== key) {
             obj[newKey] = obj[key]
